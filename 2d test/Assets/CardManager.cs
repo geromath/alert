de850230857345c43d12c282df1426d769f02af1
geromath <mathiams@stud.ntnu.no>
2018-02-23 10:35:48 +0100

    huehuehue

---
+++ 2d test/Assets/CardManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardManager : MonoBehaviour {
+
+	public GameObject card;
+	public float spawnTime = 2f;
+	public Transform[] spawnPoints;
+
+	void Start () {
+		InvokeRepeating("Spawn", spawnTime, spawnTime);
+	}
+
+	void Spawn () {
+
+		Debug.Log("Spawning");
+
+		int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+
+		Instantiate(card, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+	}
+}
