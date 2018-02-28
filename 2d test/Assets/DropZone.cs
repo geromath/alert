b46b2f1373f4e51ca2978f6c0ccf5e55762de997
geromath <mathiams@stud.ntnu.no>
2018-02-28 10:59:23 +0100

    crappy score system, might need a change

--- 2d test/Assets/DropZone.cs
+++ 2d test/Assets/DropZone.cs
@@ -2,8 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class DropZone : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler {
+
+	public Text scoreText;
+	public int score = 0;
 
 	public void OnPointerEnter (PointerEventData eventData) {
 	}
@@ -17,6 +21,9 @@ public class DropZone : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPoin
 		Draggable d = eventData.pointerDrag.GetComponent<Draggable>();
 		if(d != null) {
 			d.originalParent = this.transform;
+			score += 10;
+			scoreText.text = score.ToString();
+			Destroy(d.gameObject);
 		}
 	}
 }
